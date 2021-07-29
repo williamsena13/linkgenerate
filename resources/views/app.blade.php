9fdb820941784098d434ddbe08c11ba4957c81fe
williamsena13 <williamsena13@gmail.com>
2021-07-29 13:55:32 -0300

    ajuste em projeto

---
+++ resources/views/app.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Vue and Lumen</title>
+    <link rel="stylesheet" href="dist/app.css" />
+</head>
+<body>
+    <h1>My App</h1>
+    <div id="app"></div>
+    <script src="dist/app.js"></script>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,9 @@
 |
 */
 
-$router->get('/', function () use ($router) {
-    return $router->app->version();
+$router->get('/', 'HomeController@index');
+
+$router->get('/{route:.*}/', function ()  {
+    return view('app');
 });
+
